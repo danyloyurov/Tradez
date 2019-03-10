d88426c1783ecbaaa2e92610019830a22af553da
Danylo Yurov <aveyurov@gmail.com>
2019-03-10 18:58:30 +0200

    [Tradez] Implements logger utility

---
+++ dev/common/utilities/include/logger.hpp
@@ -0,0 +1,26 @@
+#ifndef LOGGER_HPP_
+#define LOGGER_HPP_
+
+#include "error_interface.hpp"
+
+#include <string>
+
+class Logger {
+    public:
+        static Logger& Instanse();
+
+        static struct File {} FileTag;
+        static struct Terminal {} TerminalTag;
+
+        void Log(const std::string& message, File);
+        void Log(const std::string& message, Terminal);
+
+        Logger(const Logger&) = delete;
+        void operator=(const Logger&) = delete;
+    private:
+        Logger();
+    
+};
+
+#endif // LOGGER_HPP_
+
