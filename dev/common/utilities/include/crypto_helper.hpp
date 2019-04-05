5276de307576dc64aa4f65a92a32df6e88836ec2
Danylo Yurov <aveyurov@gmail.com>
2019-04-05 22:39:44 +0300

    [TradingClient] Declaration of client interfaces

---
+++ dev/common/utilities/include/crypto_helper.hpp
@@ -0,0 +1,9 @@
+#ifndef CRYPTO_HELPER_HPP_
+#define CRYPTO_HELPER_HPP_
+
+class CryptoHelper {
+    public:
+
+};
+
+#endif // CRYPTO_HELPER_HPP_
---
+++ dev/trading_platforms/common/client_interface.hpp
@@ -0,0 +1,13 @@
+#ifndef CLIENT_INTERFACE_HPP_
+#define CLIENT_INTERFACE_HPP_
+
+#include "error_interface.hpp"
+
+template<class T>
+class ClientInterface {
+    public:
+      virtual error::TradingError PublicRequest(const T& request) = 0;
+      virtual error::TradingError PrivateRequest(const T& request) = 0;
+};
+
+#endif // CLIENT_INTERFACE_HPP_
